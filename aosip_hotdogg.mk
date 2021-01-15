203259041b1b4321148838f37773975844d76f1a
Matt Evangelisti <mjevange@gmail.com>
2021-01-15 13:26:05 -0500

    hotdogg: Upate FP with Coral

--- aosip_hotdogg.mk
+++ aosip_hotdogg.mk
@@ -30,7 +30,7 @@ TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
 # Build info
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7TProNR \
     PRODUCT_NAME=OnePlus7TProNR \
